db1f7ff1897c7d2b906f4c693fd0530371303112
Phil Seeley <phil.seeley@gmail.com>
2024-05-30 09:25:20 +0100

    First cut of "drawn" wind rose.

--- lib/widgets/wind_rose_box.dart
+++ lib/widgets/wind_rose_box.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:boatinstrument/boatinstrument_controller.dart';
 
@@ -5,6 +7,67 @@ enum WindRoseType {
   normal,
   closeHaul,
   auto
+}
+
+class _RosePainter extends CustomPainter {
+  final BuildContext _context;
+  final WindRoseType _type;
+
+  _RosePainter(this._context, this._type);
+
+  @override
+  void paint(Canvas canvas, Size canvasSize) {
+    Color fg = Theme.of(_context).colorScheme.onSurface;
+    double size = min(canvasSize.width, canvasSize.height);
+    Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = fg
+      ..strokeWidth = 2.0;
+
+    canvas.drawCircle(Offset(size/2, size/2), size/2, paint);
+    paint..strokeWidth = 20.0..color = Colors.green;
+    canvas.drawArc(const Offset(10.0, 10.0) & Size(size-20.0, size-20.0), deg2Rad(20)-(pi/2), deg2Rad(40), false, paint);
+    paint.color = Colors.red;
+    canvas.drawArc(const Offset(10.0, 10.0) & Size(size-20.0, size-20.0), deg2Rad(300)-(pi/2), deg2Rad(40), false, paint);
+    paint.color = fg;
+    for(int a = 0; a < 360; a += 10) {
+      paint.strokeWidth = 10.0;
+      if (a % 30 == 0) {
+        paint.strokeWidth = 20.0;
+      }
+      canvas.drawArc(const Offset(10.0, 10.0) & Size(size-20.0, size-20.0), deg2Rad(a)-(pi/2)-0.005, 0.01, false, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+class _NeedlePainter extends CustomPainter {
+
+  final Color _color;
+  final double _angle;
+
+  _NeedlePainter(this._color, this._angle);
+
+  @override
+  void paint(Canvas canvas, Size canvasSize) {
+    double size = min(canvasSize.width, canvasSize.height);
+    Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = _color
+      ..strokeWidth = 10.0;
+
+    Path needle = Path()
+      // ..moveTo(size/2, size/2)
+      ..lineTo(0, size/2);
+    canvas.translate(size/2, size/2);
+    canvas.rotate(_angle+pi);
+    canvas.drawPath(needle, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
 class WindRoseCHBox extends WindRoseBox {
@@ -31,11 +94,6 @@ class WindRoseBox extends BoxWidget {
 class _WindRoseBoxState extends State<WindRoseBox> {
   double? _windAngleApparent;
   double? _windAngleTrue;
-  //TODO get/make proper images. Can these be vector images?
-  static const Image _rose = Image(image: AssetImage('assets/wind-rose.png'));
-  static const Image _roseCH = Image(image: AssetImage('assets/wind-rose-ch.png'));
-  static const Image _apparentNeedle = Image(color: Colors.red, image: AssetImage('assets/wind-needle.png'));
-  static const Image _trueNeedle = Image(color: Colors.yellow, image: AssetImage('assets/wind-needle.png'));
 
   @override
   void initState() {
@@ -48,27 +106,21 @@ class _WindRoseBoxState extends State<WindRoseBox> {
 
   @override
   Widget build(BuildContext context) {
-    Image rose = _rose;
     double angleApparent = _windAngleApparent??0.0;
     double angleTrue = _windAngleTrue??0.0;
 
-    switch(widget._type) {
-      case WindRoseType.normal:
-        break;
-      case WindRoseType.closeHaul: //TODO this needs lots of work
-        rose = _roseCH;
-        angleApparent = (angleApparent < 0.524) ? angleApparent*0.524 : angleApparent;
-        angleTrue = (angleTrue < 0.524) ? angleTrue*0.524 : angleTrue;
-        break;
-      case WindRoseType.auto:
-        // TODO: Handle this case.
+    List<Widget> stack = [
+      CustomPaint(size: Size.infinite, painter: _RosePainter(context, widget._type))
+    ];
+
+    if(_windAngleTrue != null) {
+      stack.add(CustomPaint(size: Size.infinite, painter: _NeedlePainter(Colors.yellow, angleTrue)));
     }
 
-    return Center(child: Stack(alignment: Alignment.center, children: [
-      rose,
-      Transform.rotate(angle: angleApparent, child: _apparentNeedle),
-      Transform.rotate(angle: angleTrue, child: _trueNeedle)
-    ]));
+    if(_windAngleApparent != null) {
+      stack.add(CustomPaint(size: Size.infinite, painter: _NeedlePainter(Colors.blue, angleApparent)));
+    }
+    return Container(padding: const EdgeInsets.all(20.0), child: Stack(children: stack));
   }
 
   _processData(List<Update> updates) {
