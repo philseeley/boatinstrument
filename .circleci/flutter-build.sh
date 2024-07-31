172f255f5bb2ff634ad0a0a4ce4b439756a0d872
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 15:58:55 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -35,8 +35,11 @@ fi
 
 cd ..
 
-chmod +x ./package
-./package linux
+flutter build linux
+cpu=$LMARCH
+cd build/linux/${cpu:0:1}*/release
+mv bundle boatinstrument
+tar czf "${package_dir}"/${name}-${type}-${cpu}.tgz boatinstrument
 
 ls -ltr build/linux/*/release/boatinstrument/*
 pwd
