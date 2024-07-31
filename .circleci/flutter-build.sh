84cd3662cd5d7daa13d593f9a235833179ed2467
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 10:55:16 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -5,6 +5,12 @@ echo "Building using Flutter"
 set -x
 
 git clone https://github.com/flutter/flutter.git -b stable
+cd flutter
+git clean -xfd
+git stash save --keep-index
+git stash drop
+git pull
+cd ..
 
 export PATH="$PATH:`pwd`/flutter/bin"
 
