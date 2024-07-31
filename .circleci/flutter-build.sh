2e7ccc5e11a4d0e1c45e0f9b8890877c63292be0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 11:56:52 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -4,6 +4,14 @@ echo "Building using Flutter"
 
 set -x
 
+# Install dar from .deb
+apt-get -q -y install apt-transport-https
+sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+apt-get -q -y update
+apt-get -q -y install dart
+
+
 git clone https://github.com/flutter/flutter.git -b stable
 cd flutter
 git clean -xfd
