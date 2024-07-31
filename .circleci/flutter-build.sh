a393752d7c75e66a9d2653b60155573473cfe3b8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 13:54:36 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -25,7 +25,7 @@ cd ..
 export PATH="$PATH:`pwd`/flutter/bin"
 
 if [ "$LMARCH" == 'armhf' ]; then
-  apt-get -q -y libarchive-dev 
+  apt-get -q -y install libarchive-dev 
   flutter doctor || true
   mkdir -p `pwd`/flutter/bin/cache/dart-sdk/
   cp -r `pwd`/dart-sdk/* `pwd`/flutter/bin/cache/dart-sdk/
