1e71a0fe9fdf20daa25668c47ee793a1a371a808
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 15:46:50 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -24,7 +24,7 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 if [ "$LMARCH" == 'armhf' ]; then
   flutter doctor -v || true
-  #rm -rf `pwd`/flutter/bin/cache/dart-sdk/
+  rm -rf `pwd`/flutter/bin/cache/dart-sdk/
   mkdir -p `pwd`/flutter/bin/cache/dart-sdk/
   cp -r `pwd`/dart-sdk/* `pwd`/flutter/bin/cache/dart-sdk/
   file `pwd`/flutter/bin/cache/dart-sdk/bin/dart
