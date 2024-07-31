04accbc5399b9106ebf5dc229b195ac70296b559
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 13:30:02 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -25,6 +25,7 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 if [ "$LMARCH" == 'armhf' ]; then
   apt-get -q -y install libc6-compat gcompat 
+  mkdir -p `pwd`/flutter/bin/cache/dart-sdk/
   cp -r `pwd`/dart-sdk/* `pwd`/flutter/bin/cache/dart-sdk/
   file `pwd`/flutter/bin/cache/dart-sdk/bin/dart
 else
