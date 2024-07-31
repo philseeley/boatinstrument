30be75b3c30145d871bc246d0b20e6dfed19d9c9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 12:16:49 -0400

    Update flutter-build.sh

--- .circleci/flutter-build.sh
+++ .circleci/flutter-build.sh
@@ -6,8 +6,12 @@ set -x
 
 # Install dart from .deb
 apt-get -q -y install apt-transport-https
-sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub \
+  | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=armhf] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' \
+  | sudo tee /etc/apt/sources.list.d/dart_stable.list
+#sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+#sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 apt-get -q -y update
 apt-get -q -y install dart
 
