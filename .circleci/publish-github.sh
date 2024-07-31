811b88b4a0e0a81c79cfd3d7149b429e09060544
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-07-31 12:41:23 -0400

    Update publish-github.sh

--- .circleci/publish-github.sh
+++ .circleci/publish-github.sh
@@ -4,3 +4,14 @@ echo "Publishing to github release"
 
 set -x
 
+FILE=$(ls packages/*.tgz)
+echo curl -X POST \
+    -H '"Content-Length: '$(stat --format=%s $FILE)'"' \
+    -H '"Content-Type: '$(file -b --mime-type $FILE)'"' \
+    -T '"'$FILE'"' \
+    -H '"Authorization: token '$GITHUB_TOKEN'"' \
+    -H '"Accept: application/vnd.github.v3+json"' \
+    '"https://uploads.github.com/repos/bareboat-necessities/lysmarine_gen/releases/54202060/assets?name='$(basename $FILE)'"' >> upload.command
+
+cat upload.command
+cat upload.command | xargs -0 -L 1 -I CMD -P 1 bash -c CMD
