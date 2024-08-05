b8ac6fc32780a0a53ccb6743a45d944b61ff4d76
Phil Seeley <phil.seeley@gmail.com>
2024-08-05 17:44:36 +0100

    Updated test to working values.

--- test/angle_test.dart
+++ test/angle_test.dart
@@ -28,22 +28,22 @@ void main() {
 
   test('Angle smoothing', ()
   {
-    expect(rad2Deg(averageAngle(deg2Rad( 10), deg2Rad(350), smooth: 10)),   1);
-    expect(rad2Deg(averageAngle(deg2Rad(350), deg2Rad( 10), smooth: 10)), 359);
-    expect(rad2Deg(averageAngle(deg2Rad( 45), deg2Rad(135), smooth: 10)),  85);
-    expect(rad2Deg(averageAngle(deg2Rad(135), deg2Rad( 45), smooth: 10)),  95);
-    expect(rad2Deg(averageAngle(deg2Rad(135), deg2Rad(225), smooth: 10)), 175);
-    expect(rad2Deg(averageAngle(deg2Rad(225), deg2Rad(135), smooth: 10)), 185);
-    expect(rad2Deg(averageAngle(deg2Rad(225), deg2Rad(315), smooth: 10)), 265);
-    expect(rad2Deg(averageAngle(deg2Rad(315), deg2Rad(225), smooth: 10)), 275);
-    expect(rad2Deg(averageAngle(deg2Rad(315), deg2Rad( 45), smooth: 10)), 355);
-    expect(rad2Deg(averageAngle(deg2Rad( 45), deg2Rad(315), smooth: 10)),   5);
+    expect(rad2Deg(averageAngle(deg2Rad( 10), deg2Rad(350), smooth: 10)),   8);
+    expect(rad2Deg(averageAngle(deg2Rad(350), deg2Rad( 10), smooth: 10)), 352);
+    expect(rad2Deg(averageAngle(deg2Rad( 45), deg2Rad(135), smooth: 10)),  51);
+    expect(rad2Deg(averageAngle(deg2Rad(135), deg2Rad( 45), smooth: 10)), 129);
+    expect(rad2Deg(averageAngle(deg2Rad(135), deg2Rad(225), smooth: 10)), 141);
+    expect(rad2Deg(averageAngle(deg2Rad(225), deg2Rad(135), smooth: 10)), 219);
+    expect(rad2Deg(averageAngle(deg2Rad(225), deg2Rad(315), smooth: 10)), 231);
+    expect(rad2Deg(averageAngle(deg2Rad(315), deg2Rad(225), smooth: 10)), 309);
+    expect(rad2Deg(averageAngle(deg2Rad(315), deg2Rad( 45), smooth: 10)), 321);
+    expect(rad2Deg(averageAngle(deg2Rad( 45), deg2Rad(315), smooth: 10)),  39);
 
-    expect(rad2Deg(averageAngle(deg2Rad(  10), deg2Rad(- 10), smooth: 10, relative: true)),    1);
-    expect(rad2Deg(averageAngle(deg2Rad(- 10), deg2Rad(  10), smooth: 10, relative: true)), -  1);
-    expect(rad2Deg(averageAngle(deg2Rad(  45), deg2Rad( 135), smooth: 10, relative: true)),   85);
-    expect(rad2Deg(averageAngle(deg2Rad( 135), deg2Rad(  45), smooth: 10, relative: true)),   95);
-    expect(rad2Deg(averageAngle(deg2Rad(- 45), deg2Rad(-135), smooth: 10, relative: true)), - 85);
-    expect(rad2Deg(averageAngle(deg2Rad(-135), deg2Rad(- 45), smooth: 10, relative: true)), - 95);
+    expect(rad2Deg(averageAngle(deg2Rad(  10), deg2Rad(- 10), smooth: 10, relative: true)),    8);
+    expect(rad2Deg(averageAngle(deg2Rad(- 10), deg2Rad(  10), smooth: 10, relative: true)), -  8);
+    expect(rad2Deg(averageAngle(deg2Rad(  45), deg2Rad( 135), smooth: 10, relative: true)),   51);
+    expect(rad2Deg(averageAngle(deg2Rad( 135), deg2Rad(  45), smooth: 10, relative: true)),  129);
+    expect(rad2Deg(averageAngle(deg2Rad(- 45), deg2Rad(-135), smooth: 10, relative: true)), - 51);
+    expect(rad2Deg(averageAngle(deg2Rad(-135), deg2Rad(- 45), smooth: 10, relative: true)), -129);
   });
 }
