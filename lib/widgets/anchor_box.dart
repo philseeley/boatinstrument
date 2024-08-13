261acfdcc49af89867bb1b6d94bb3cae645fa721
Phil Seeley <phil.seeley@gmail.com>
2024-08-13 13:20:50 +0100

    The AnchorAlarmBox can now move the anchor position.

--- lib/widgets/anchor_box.dart
+++ lib/widgets/anchor_box.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:math' as m;
 
@@ -5,6 +6,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:json_annotation/json_annotation.dart';
+import 'package:latlong2/latlong.dart' as ll;
 import 'package:nanoid/nanoid.dart';
 
 import '../authorization.dart';
@@ -26,7 +28,7 @@ class _AnchorAlarmSettings {
 class _AnchorPainter extends CustomPainter {
   final BuildContext _context;
   final BoatInstrumentController _controller;
-  final int _maxRadius;
+  final int? _maxRadius;
   final int _currentRadius;
   final double? _bearingTrue;
   final double _apparentBearing;
@@ -38,6 +40,7 @@ class _AnchorPainter extends CustomPainter {
     Color maxColor = _controller.val2PSColor(_context, -1, none: Colors.grey);
     Color currentColor = _controller.val2PSColor(_context, 1, none: Colors.grey);
     TextStyle th = Theme.of(_context).textTheme.bodyMedium!;
+    TextPainter tp = TextPainter(textDirection: TextDirection.ltr);
 
     double size = m.min(canvasSize.width, canvasSize.height) /2;
 
@@ -46,25 +49,32 @@ class _AnchorPainter extends CustomPainter {
       ..color = maxColor
       ..strokeWidth = 2.0;
 
-    canvas.drawCircle(Offset(size, size), size, paint);
+    double ratio = _currentRadius/(_maxRadius??_currentRadius)*size;
 
-    double ratio = (_currentRadius/_maxRadius)*size;
+    if(_maxRadius != null) {
+      canvas.drawCircle(Offset(size, size), size, paint);
+      tp.text = TextSpan(text: _maxRadius.toString(), style: th.copyWith(backgroundColor: maxColor));
+      tp.layout();
+      tp.paint(canvas, Offset((size*2)-tp.size.width, size-tp.size.height/2));
+    }
 
     paint.color = currentColor;
     canvas.drawCircle(Offset(size, size), ratio, paint);
+    tp.text = TextSpan(text: _currentRadius.toString(), style: th.copyWith(backgroundColor: currentColor));
+    tp.layout();
+    tp.paint(canvas, Offset(size-ratio, size-tp.size.height/2));
 
-    TextPainter tp = TextPainter(textDirection: TextDirection.ltr);
+    IconData icon = Icons.anchor;
+    tp.text = TextSpan(text: String.fromCharCode(icon.codePoint),
+        style: TextStyle(fontSize: 30,
+            fontFamily: icon.fontFamily,
+            color: currentColor));
+    tp.layout();
+    tp.paint(canvas, Offset(size-tp.size.width / 2, size-tp.size.height / 2));
 
     if(_bearingTrue != null) {
       canvas.save();
       canvas.translate(size, size);
-      IconData icon = Icons.anchor;
-      tp.text = TextSpan(text: String.fromCharCode(icon.codePoint),
-          style: TextStyle(fontSize: 30,
-              fontFamily: icon.fontFamily,
-              color: currentColor));
-      tp.layout();
-      tp.paint(canvas, Offset(-tp.size.width / 2, -tp.size.height / 2));
       canvas.rotate(_bearingTrue! - m.pi);
       canvas.translate(0, ratio);
       canvas.rotate((m.pi/2) - _apparentBearing);
@@ -78,14 +88,8 @@ class _AnchorPainter extends CustomPainter {
       canvas.drawLine(Offset.zero, Offset(-size, 0), paint);
       canvas.restore();
     }
-
-    tp.text = TextSpan(text: _maxRadius.toString(), style: th.copyWith(backgroundColor: maxColor));
-    tp.layout();
-    tp.paint(canvas, Offset((size*2)-tp.size.width, size-tp.size.height/2));
-    tp.text = TextSpan(text: _currentRadius.toString(), style: th.copyWith(backgroundColor: currentColor));
-    tp.layout();
-    tp.paint(canvas, Offset(size-ratio, size-tp.size.height/2));
   }
+
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
@@ -120,11 +124,15 @@ class _AnchorState extends State<AnchorAlarmBox> {
   static const emergencyState = 'emergency';
 
   late final _AnchorAlarmSettings _settings;
+  ll.LatLng? _anchorPosition;
   int? _maxRadius;
   int? _currentRadius;
   double? _bearingTrue;
   double? _apparentBearing;
   bool _silenceAlarm = false;
+  bool _unlocked = false;
+  Offset _startDragOffset = Offset.zero;
+  Timer? _lockTimer;
   AudioPlayer player = AudioPlayer();
 
   @override
@@ -149,24 +157,70 @@ class _AnchorState extends State<AnchorAlarmBox> {
     Color dropColor = widget.config.controller.val2PSColor(context, 1, none: Colors.grey);
     Color raiseColor = widget.config.controller.val2PSColor(context, -1, none: Colors.grey);
 
+    if(_currentRadius != null && _maxRadius == null) {
+      _toggleMove(allowed: true);
+    }
+
     List<Widget> col = [
       Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
         IconButton(onPressed: _toggleAlarm, icon: Icon(_silenceAlarm ? Icons.notifications_off_outlined : Icons.notifications_outlined, color: dropColor)),
-        IconButton(onPressed: _drop, icon: Icon(Icons.keyboard_double_arrow_down, color: dropColor)),
-        IconButton(onPressed: _setRadius, icon: Icon(Icons.circle_outlined, color: dropColor)),
+        IconButton(onPressed: _maxRadius == null ? _drop : null, icon: Icon(Icons.anchor, color: dropColor)),
+        IconButton(onPressed: _toggleMove, icon: Icon(_unlocked ? Icons.lock_open : Icons.lock, color: dropColor)),
+        IconButton(onPressed: _unlocked ? _setRadius : null, icon: Icon(Icons.highlight_off, color: dropColor)),
         IconButton(onPressed: () {_changeRadius(-5);}, icon: Icon(Icons.remove, color: dropColor)),
         IconButton(onPressed: () {_changeRadius(5);}, icon: Icon(Icons.add, color: dropColor)),
-        IconButton(onPressed: _raise, icon: Icon(Icons.keyboard_double_arrow_up, color: raiseColor)),
+        IconButton(onPressed: _unlocked ? _raise : null, icon: Stack(children: [Icon(Icons.anchor, color: raiseColor), Icon(Icons.close, color: raiseColor)])),
       ]),
     ];
 
-    if(_maxRadius != null && _currentRadius != null) {
+    if(_currentRadius != null) {
       col.add(Expanded(
-          child: RepaintBoundary(child: CustomPaint(size: Size.infinite,
-              painter: _AnchorPainter(widget.config.controller, context, _maxRadius!, _currentRadius!, _bearingTrue, _apparentBearing??0)))),
+          child: GestureDetector(onPanDown: _unlocked ? _startDrag : null, onPanEnd: _unlocked ? _stopDrag : null,
+              child: RepaintBoundary(child: CustomPaint(size: Size.infinite,
+                painter: _AnchorPainter(widget.config.controller, context, _maxRadius, _currentRadius!, _bearingTrue, _apparentBearing??0))))),
       );
     }
     return Padding(padding: const EdgeInsets.all(5), child: Column(children: col));
+  }
+
+  void _startDrag(DragDownDetails details) {
+    _setLockTimer();
+    _startDragOffset = details.localPosition;
+  }
+
+  void _stopDrag(DragEndDetails details) {
+    _setLockTimer();
+    Offset diff = details.localPosition - _startDragOffset;
+    double size = m.min(widget.config.constraints.maxWidth,
+        widget.config.constraints.maxHeight) / 2;
+
+    double ratio = (_maxRadius??_currentRadius!) / size;
+
+    ll.LatLng newPosition = const ll.Distance().offset(
+        _anchorPosition!, diff.distance * ratio,
+        rad2Deg(diff.direction + (m.pi / 2)));
+
+    _sendCommand('setAnchorPosition',
+        '{"position": {"latitude": ${newPosition.latitude}, "longitude": ${newPosition.longitude}}}');
+  }
+
+  void _toggleMove ({bool? allowed}) {
+    setState(() {
+      _unlocked = allowed??!_unlocked;
+    });
+    _setLockTimer();
+  }
+
+  void _setLockTimer() {
+    _lockTimer?.cancel();
+    if(_unlocked) {
+      _lockTimer = Timer(const Duration(minutes: 2), () {
+        _unlocked = false;
+        if(mounted) {
+          setState(() {});
+        }
+      });
+    }
   }
 
   void _toggleAlarm () {
@@ -174,14 +228,17 @@ class _AnchorState extends State<AnchorAlarmBox> {
       _silenceAlarm = !_silenceAlarm;
     });
     if(_silenceAlarm) {
+      ScaffoldMessenger.of(context).clearSnackBars();
       player.stop();
     }
   }
+
   void _drop() {
     _sendCommand('dropAnchor', '');
   }
 
   void _setRadius() {
+    _setLockTimer();
     _sendCommand('setRadius', '');
   }
 
@@ -235,7 +292,7 @@ class _AnchorState extends State<AnchorAlarmBox> {
         player.stop();
       }
       widget.config.controller.showMessage(context,
-          'Anchor: $state',
+          'Anchor Alarm: $state',
           millisecondsDuration: emergency ? 500 : 4000,
           error: emergency);
     }
@@ -248,16 +305,21 @@ class _AnchorState extends State<AnchorAlarmBox> {
       for (Update u in updates) {
         try {
           switch (u.path) {
+            case 'navigation.anchor.position':
+              _anchorPosition = ll.LatLng((u.value['latitude'] as num).toDouble(), (u.value['longitude'] as num).toDouble());
+              break;
             case 'navigation.anchor.maxRadius':
               try {
-                _maxRadius = (u.value as num).toInt();
+                _maxRadius = (u.value as num).round();
               } catch (_){
                 //TODO this only happens if the Anchor Alarm webapp is used.
                 _maxRadius = int.parse(u.value as String);
               }
               break;
             case 'navigation.anchor.currentRadius':
-              _currentRadius = (u.value as num).toInt();
+              _currentRadius = (u.value as num).round();
+              // Make sure we have a radius to avoid div-by-zero error.
+              _currentRadius = _currentRadius == 0 ? 1 : _currentRadius;
               break;
             case 'navigation.anchor.bearingTrue':
               _bearingTrue = (u.value as num).toDouble()-m.pi;
