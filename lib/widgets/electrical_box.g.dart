924973496ebda75b9ea81b94a5c27cbe0c9f54b2
Phil Seeley <phil.seeley@gmail.com>
2024-12-26 11:11:54 +0000

    BatteryVoltMeterBox: you can now select the nominal battery voltage.

--- lib/widgets/electrical_box.g.dart
+++ lib/widgets/electrical_box.g.dart
@@ -15,3 +15,24 @@ Map<String, dynamic> _$ElectricalSettingsToJson(_ElectricalSettings instance) =>
     <String, dynamic>{
       'id': instance.id,
     };
+
+_ElectricalBatterySettings _$ElectricalBatterySettingsFromJson(
+        Map<String, dynamic> json) =>
+    _ElectricalBatterySettings(
+      id: json['id'] as String? ?? '',
+      voltage: $enumDecodeNullable(_$BatteryVoltageEnumMap, json['voltage']) ??
+          BatteryVoltage.twelve,
+    );
+
+Map<String, dynamic> _$ElectricalBatterySettingsToJson(
+        _ElectricalBatterySettings instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'voltage': _$BatteryVoltageEnumMap[instance.voltage]!,
+    };
+
+const _$BatteryVoltageEnumMap = {
+  BatteryVoltage.twelve: 'twelve',
+  BatteryVoltage.twentyFour: 'twentyFour',
+  BatteryVoltage.fortyEight: 'fortyEight',
+};
