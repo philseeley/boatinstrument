8a53e59f73f83515060b3afb6514b9ae0df94471
Phil Seeley <phil.seeley@gmail.com>
2026-08-02 10:10:59 +0200

    Added more info when discarding old data.

--- lib/boatinstrument_controller.dart
+++ lib/boatinstrument_controller.dart
@@ -1395,7 +1395,7 @@ class BoatInstrumentController {
                     bd.updates.add(Update(context, path,value));
                     bd.pathTimestamps[path] = now;
                   } else {
-                   l.i('Discarding old data for "$u"');
+                   l.i('Discarding old data now: "$now", diff: "$d", update: "$u"');
                   }
                 }
               }
