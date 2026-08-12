a288bdc5f8d01135a67f27a39104a4ba6345078a
Phil Seeley <phil.seeley@gmail.com>
2026-08-12 15:04:21 +0200

    Added option for Bar Gauges to colour the bar with the range colours.
    Applied this to the Battery State of Charge Box.

--- lib/widgets/gauge_box.dart
+++ lib/widgets/gauge_box.dart
@@ -392,9 +392,10 @@ class _BarGaugePainter extends CustomPainter {
   final int _step;
   final List<GaugeRange> _ranges;
   final Color _barColor;
+  final bool _backgroundRanges;
   final double? _value;
 
-  _BarGaugePainter(this._context, this._minValue, this._maxValue, this._step, this._ranges, this._barColor, this._value);
+  _BarGaugePainter(this._context, this._minValue, this._maxValue, this._step, this._ranges, this._barColor, this._backgroundRanges, this._value);
 
   @override
   void paint(Canvas canvas, Size canvasSize) {
@@ -407,13 +408,22 @@ class _BarGaugePainter extends CustomPainter {
 
     double step = h/(_maxValue - _minValue);
 
-    for(GaugeRange r in _ranges) {
-      paint.color = r.color;
-      canvas.drawRect(Rect.fromLTRB(0, h-(step*(r.max-_minValue)), w, h-(step*(r.min - _minValue))), paint);
+    if(_backgroundRanges) {
+      for(GaugeRange r in _ranges) {
+        paint.color = r.color;
+        canvas.drawRect(Rect.fromLTRB(0, h-(step*(r.max-_minValue)), w, h-(step*(r.min - _minValue))), paint);
+      }
     }
 
     if(_value != null) {
-      paint.color = _barColor;
+      var c = _barColor;
+      if(!_backgroundRanges) {
+        for(GaugeRange r in _ranges) {
+          if(_value > r.min && _value <= r.max) c = r.color;
+        }
+      }
+      
+      paint.color = c;
       canvas.drawRect(Rect.fromLTRB(35, h-(step*(_value-_minValue)), w, h), paint);
     }
 
@@ -445,9 +455,10 @@ class _BarGaugePainter extends CustomPainter {
 abstract class DoubleValueBarGaugeBox extends DoubleValueGaugeBox {
   final bool showPercent;
   final Color barColor;
+  final bool backgroundRanges;
 
   const DoubleValueBarGaugeBox(super.config, super.title, super.path,
-    {this.showPercent = false, this.barColor = Colors.blue, super.minValue = 0, required super.maxValue, required super.step,
+    {this.showPercent = false, this.barColor = Colors.blue, this.backgroundRanges = true, super.minValue = 0, required super.maxValue, required super.step,
     super.ranges, super.smoothing, super.dataType, super.key});
 
   @override
@@ -465,7 +476,7 @@ class DoubleValueBarGaugeBoxState<T extends DoubleValueBarGaugeBox> extends Doub
       Expanded(child: Padding(padding: const EdgeInsets.only(left: pad, right: pad),
         child: RepaintBoundary(child: CustomPaint(
           size: Size.infinite,
-          painter: _BarGaugePainter(context, minDisplay, maxDisplay, _displayStep, _displayRanges, widget.barColor, displayValue)
+          painter: _BarGaugePainter(context, minDisplay, maxDisplay, _displayStep, _displayRanges, widget.barColor, widget.backgroundRanges, displayValue)
       )))),
       Row(children: [Padding(padding: const EdgeInsets.all(pad), child: Text(widget.units(value??0), style: style))]),
     ]);
